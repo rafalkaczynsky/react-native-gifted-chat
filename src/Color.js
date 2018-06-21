d6010b309a3e9994ea896c9543d2b544e5c654b0
rafalkaczynsky <rafalkaczynsky1985@gmail.com>
2018-06-21 14:50:42 +0100

    Update Color.js

--- src/Color.js
+++ src/Color.js
@@ -2,7 +2,7 @@ export default {
   defaultColor: '#b2b2b2',
   backgroundTransparent: 'transparent',
   defaultBlue: '#0084ff',
-  leftBubbleBackground: '#f0f0f0',
+  leftBubbleBackground: '#ffffff',
   white: '#fff',
   carrot: '#e67e22',
   emerald: '#2ecc71',
